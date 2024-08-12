dab00df7db265de23d020a2afe2035daf911cf0f
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-08-12 08:14:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const mysql = require("mysql2");
 
 const app = express();
 app.listen(3000, ()=>{
-    
+
     console.log("Servidor em Execução");
 
 })
+
+//criação da conexão com o banco de dados
+
+const connection = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'user_bd_tasks',
+
+})
+  
+
+    
